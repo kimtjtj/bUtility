2593d6a9a03e2685119d9213dc4567d4fc1035a9
taejun kim <taejun.kim@zepetto.com>
2017-01-17 17:25:50 +0900

    배치 파일 추가

---
+++ Build.bat
@@ -0,0 +1,23 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo 관리 권한을 요청 ...
+    goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    rem del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+pushd "%CD%"
+    CD /D "%~dp0"
+
+
+taskkill /f /im bUtility.exe
+
+"c:\program files (x86)\autohotkey\compiler\ahk2exe" /in "bUtility.ahk"
